c3fb70717ea90342baf3bb8934a224cb64738cdd
Troy Wilson <hello@troywilson.io>
2020-10-27 22:55:27 +1100

    hover effect with out of stock disabling button

--- main.js
+++ main.js
@@ -4,24 +4,40 @@ const app = Vue.createApp({
 			cart: 0,
 			product: 'Socks',
 			brand: 'Vue Mastery',
-			image: './assets/images/socks_blue.jpg',
-			inStock: false,
+			selectedVariant: 0,
 			details: [ '50% cotton', '30% wool', '20% polyester' ],
 			variants: [
-				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-				{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
-			]
+				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+				{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+			],
+			// solution
+			onSale: true
+			// solution
 		};
 	},
 	methods: {
 		addToCart() {
 			this.cart += 1;
 		},
-		removeItem() {
-			this.cart -= 1;
+		updateVariant(index) {
+			this.selectedVariant = index;
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product;
 		},
-		updateImage(variantImage) {
-			this.image = variantImage;
+		image() {
+			return this.variants[this.selectedVariant].image;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].quantity;
+		},
+		sale() {
+			if (this.onSale) {
+				return this.brand + ' ' + this.product + ' is on sale.';
+			}
+			return '';
 		}
 	}
 });
