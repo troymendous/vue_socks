c43f3380a14a8d0edb89142efe9d14900e722f54
Troy Wilson <hello@troywilson.io>
2020-10-31 21:10:17 +1100

    refactoring and cleaning

--- Components/ProductDisplay.js
+++ Components/ProductDisplay.js
@@ -6,35 +6,42 @@ app.component('product-display', {
 		}
 	},
 	/*html*/
-
 	template: `<div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <p>Shipping {{ shipping }}</p>
-        
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }">
-        </div>
-        
-        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
-      </div>
-    </div>
-  </div>`,
+	  <div class="product-container">
+		<div class="product-image">
+		  <img v-bind:src="image">
+		</div>
+		<div class="product-info">
+		  <h1>{{ title }}</h1>
+  
+		  <p v-if="inStock">In Stock</p>
+		  <p v-else>Out of Stock</p>
+  
+		  <p>Shipping: {{ shipping }}</p>
+		  <ul>
+			<li v-for="detail in details">{{ detail }}</li>
+		  </ul>
+  
+		  <div 
+			v-for="(variant, index) in variants" 
+			:key="variant.id" 
+			@mouseover="updateVariant(index)" 
+			class="color-circle" 
+			:style="{ backgroundColor: variant.color }">
+		  </div>
+		  
+		  <button 
+			class="button" 
+			:class="{ disabledButton: !inStock }" 
+			:disabled="!inStock" 
+			v-on:click="addToCart">
+			Add to Cart
+		  </button>
+		</div>
+	  </div>
+	  <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+	  <review-form @review-submitted="addReview"></review-form>
+	</div>`,
 	data() {
 		return {
 			product: 'Socks',
@@ -44,15 +51,19 @@ app.component('product-display', {
 			variants: [
 				{ id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
 				{ id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-			]
+			],
+			reviews: []
 		};
 	},
 	methods: {
 		addToCart() {
-			this.cart += 1;
+			this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
 		},
 		updateVariant(index) {
 			this.selectedVariant = index;
+		},
+		addReview(review) {
+			this.reviews.push(review);
 		}
 	},
 	computed: {
@@ -63,7 +74,7 @@ app.component('product-display', {
 			return this.variants[this.selectedVariant].image;
 		},
 		inStock() {
-			return this.variants[this.selectedVariant].image;
+			return this.variants[this.selectedVariant].quantity;
 		},
 		shipping() {
 			if (this.premium) {
